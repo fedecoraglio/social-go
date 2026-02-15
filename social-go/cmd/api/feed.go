adc0007555819aeafb067ebf5daa0408509c9113
Federico Luis Coraglio <federicocoraglio@gmail.com>
2026-02-15 18:05:01 -0300

    Adding pagination to feed endpoint

--- social-go/cmd/api/feed.go
+++ social-go/cmd/api/feed.go
@@ -1,11 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"social/internal/store"
+)
 
 func (app *Application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
+	fq := store.PaginatedFeedQuery{
+		Limit:  10,
+		Offset: 0,
+		Sort:   "desc",
+	}
+	fq, err := fq.Parse(r)
+
+	if err != nil {
+		app.badRequestResponseError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponseError(w, r, err)
+		return
+	}
 
 	ctx := r.Context()
-	posts, err := app.store.Posts.GetUserFeed(ctx, int64(311))
+	posts, err := app.store.Posts.GetUserFeed(ctx, int64(311), fq)
 
 	if err != nil {
 		app.internalServerError(w, r, err)
--- social-go/internal/store/posts.go
+++ social-go/internal/store/posts.go
@@ -29,7 +29,7 @@ type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMetadata, error) {
+func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetadata, error) {
 	query := `
 		SELECT p.id, p.user_id, p.content, p.title,p.created_at, p.version, p.tags, u.username,
 			   COUNT(c.id) as comments_count
@@ -38,10 +38,11 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMe
 					 JOIN Followers f ON f.follower_id = p.user_id OR p.user_id = $1
 		WHERE f.user_id = $1 OR p.user_id = $1
 		GROUP BY p.id, u.username
-		ORDER BY p.created_at DESC
+		ORDER BY p.created_at ` + fq.Sort + `
+		LIMIT $2 OFFSET $3
 	`
 
-	rows, err := s.db.QueryContext(ctx, query, userID)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
 	if err != nil {
 		return nil, err
 	}
